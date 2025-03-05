0fa8e40214f6f9561cc637f058fbe974debc994e
Anastasijag12 <gudeskaanastasija@yahoo.com>
2025-03-05 11:18:13 +0100

    Dodadeni fajlovi za NODE.JS

---
+++ c01/index.js
@@ -0,0 +1,12 @@
+const {
+    convertMilesToKm,
+    convertCelsuisToFahrenheit,
+    convertPoundsToKilogram,
+    convertFeetToMetres,
+} = requere("./conversion");
+
+console.log(convertMilesToKm(500));
+console.log(convertCelsiusToFahrenheit(25));
+console.log(convertPoundsToKilogram(850));
+console.log(convertFeetToMetres(3200));
+
